d2662111f9260f469dff4997ba197643c9b383e4
Denis Novikov <alpden550@gmail.com>
2025-03-10 15:36:16 +0300

    feat(env): implement environment

---
+++ src/environment.rs
@@ -0,0 +1,28 @@
+use crate::errors::RuntimeError;
+use crate::models::literals::Literal;
+use crate::models::tokens::Token;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Environment {
+    values: HashMap<String, Literal>,
+}
+
+impl Environment {
+    pub fn new() -> Self {
+        Self {
+            values: HashMap::new(),
+        }
+    }
+
+    pub fn define(&mut self, name: String, value: Literal) {
+        self.values.insert(name, value);
+    }
+
+    pub fn get(&self, token: &Token) -> Result<Literal, RuntimeError> {
+        self.values
+            .get(&token.lexeme)
+            .cloned()
+            .ok_or_else(|| RuntimeError::UndefinedVariable(token.line, token.clone()))
+    }
+}
